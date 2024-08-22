a59af5e6ff068918ad86d9bb5f87de592838c06c
Matias-Sanmiguel <masanmigue@uade.edu.ar>
2024-08-22 11:35:36 -0300

    Traductor CSV-Matriz con pandas

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+#Lee el csv por pandas y lo traduce a matrices
+def matrix_read():
+    df = pd.read_csv('prueba.csv')
+    matrix = df.values
+    print(matrix)
+    return matrix
+
+
