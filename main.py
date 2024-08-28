3c976b2a73bb21c4820537520ab3ca5b092663a3
Matias-Sanmiguel <masanmigue@uade.edu.ar>
2024-08-28 18:55:01 -0300

    Added main menu, checker, integer checker and did minor changes

--- main.py
+++ main.py
@@ -8,7 +8,50 @@ def matrix_read():
     print(matrix)
     return matrix
 
+#def ag_el_s():
+
+#def ag_el_a():
+
+#def consulta():
+
+
+def checker_opt(elc, lista):
+    if elc in lista:
+        return 1
+    else:
+        print("Por favor, elija una opción disponible")
+        return 0
+
+def convint(elec):
+    try:
+        eleccion = int(elec)
+        return eleccion
+    except ValueError:
+        print("Valor Inválido")
+    
+### despues agregar una opcion para poder salir de una
+def askoptions(matrix):
+    flag = 0
+    opciones_posibles=[1, 2, 3, 99]
+    while flag != 1:
+        print("""
+              
+              AGREGAR / ELIMINAR stock: 1
+              AGREGAR / ELIMINAR articulos: 2
+              CONSULTAR stock: 3 
+              Salir: 99
+              """)
+        print("Elija una opción: ")
+        eleccion_pre_int = input()
+        eleccion = convint(eleccion_pre_int)
+        
+        flag = checker_opt(eleccion, opciones_posibles)
+        
+        
+    if eleccion == 99:
+        print("""Gracias por usar nuestro software
+              Saliendo...""")
+
 
 matrix = matrix_read()
-
-print(matrix)
+askoptions(matrix)
