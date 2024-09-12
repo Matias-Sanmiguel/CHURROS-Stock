773fc75e245a937494632344f973588449bc1b21
Matias-Sanmiguel <masanmigue@uade.edu.ar>
2024-09-12 10:20:34 -0300

    Del Consultas

--- consulta.py
+++
@@ -1,7 +0,0 @@
-def consulta(matrix, articulo):
-    for fila in matrix:
-        if fila[0] == articulo:
-            print(f"El stock actual de '{articulo}' es: {fila[1]}")
-            return fila[1]
-    print(f"El artículo '{articulo}' no fue encontrado en el stock.")
-    return None
--- consultaprod.py
+++
@@ -1,7 +0,0 @@
-def consultaprodd(matrix, producto):
-    for fila in matrix:
-        if fila[0] == producto and fila[2] == "Producto":
-            print(f"El stock actual del producto '{producto}' es: {fila[1]}")
-            return fila[1]
-    print(f"El producto {producto}' no fue encontrado en el stock")
-    return None
